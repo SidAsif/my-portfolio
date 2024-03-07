d34e933d8cd77257a0c96735fcbb7ea77ff298ed
SidAsif <asif050md@gmail.com>
2024-03-08 01:26:13 +0530

    floating-menu

--- script.js
+++ script.js
@@ -189,42 +189,17 @@ function getProjectsPerPage() {
 
 // Show the first page initially
 showPage(currentPage);
-
-// Attach event listeners to pagination buttons
 document.getElementById("prevPage").addEventListener("click", goToPreviousPage);
 document.getElementById("nextPage").addEventListener("click", goToNextPage);
-
-// Update the number of projects per page when the window is resized
 window.addEventListener("resize", function () {
   showPage(currentPage);
 });
 
-//floating menu // Get all menu from document
-// document.querySelectorAll(".fabTrigger").forEach(OpenMenu);
-
-// // Menu Open and Close function
-// function OpenMenu(active) {
-//   if (active.classList.contains("fabTrigger") === true) {
-//     active.addEventListener("click", function (e) {
-//       e.preventDefault();
-
-//       if (this.parentElement.classList.contains("active") === true) {
-//         // Close the clicked dropdown
-//         this.parentElement.classList.remove("active");
-//       } else {
-//         // Close the opend dropdown
-//         closeMenu();
-//         // add the open and active class(Opening the DropDown)
-//         this.parentElement.classList.add("active");
-//       }
-//     });
-//   }
-// }
-
-// // Close the openend Menu
-// function closeMenu() {
-//   // remove the open and active class from other opened Moenu (Closing the opend Menu)
-//   document.querySelectorAll(".fab").forEach(function (container) {
-//     container.classList.remove("active");
-//   });
-// }
+// float
+document.querySelector(".floating-btn").addEventListener("click", function () {
+  document.querySelector(".element-container").classList.toggle("open");
+});
+document.querySelector(".floating-btn").addEventListener("click", function () {
+  // Add a class to the floating container
+  document.querySelector(".floating-container").classList.add("expanded");
+});
